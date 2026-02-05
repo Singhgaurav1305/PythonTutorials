9bb24e43c21dbc504a78f9e5ae5ed3dbd8902c65
Gaurav Singh <thakurgaurav1305@gmail.com>
2026-02-05 15:28:12 +0530

    type

---
+++ Chapter 2/type.py
@@ -0,0 +1,18 @@
+a = 4
+b = type(a)
+print(b) # Output: <class 'int'>
+
+c = 3.14
+d = type(c)
+print(d) # Output: <class 'float'>
+
+# type casting
+
+e = "Gaurav"
+f = type(e)
+print(f) # Output: <class 'str'>
+
+g = "123"
+h = int(g) # type casting string to integer
+g = type(h)
+print(g) # Output: <class 'int'>
