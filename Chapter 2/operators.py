51d6cc67fed3ce1a5079554c259de0e46cac8630
Gaurav Singh <thakurgaurav1305@gmail.com>
2026-02-05 15:08:51 +0530

    operators

---
+++ Chapter 2/operators.py
@@ -0,0 +1,45 @@
+# Arithmetic Operators
+
+a = 10
+b = 10
+c = a + b
+print(c)  # Output: 20
+
+
+
+# Assignment Operators
+
+a = 3-2   # assign 3-2 in a variable
+print(a)  # Output: 1
+b = 8
+b += 2    # equivalent to b = b + 2
+print(b)  # Output: 10
+
+
+
+# Comparison Operators
+
+a= 5>7  # 
+print(a)    # Output: False
+b = 8!=9
+print(b)    # Output: True
+
+
+
+# Logical Operators
+
+e = True or False
+
+# Truth table of 'or' 
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth table of 'and' 
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+print(not(True))
