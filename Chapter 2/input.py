76b812008a18aaa5443427b7849755bfad67fc1f
Gaurav Singh <thakurgaurav1305@gmail.com>
2026-02-05 15:34:24 +0530

    input() function

---
+++ Chapter 2/input.py
@@ -0,0 +1,8 @@
+# input() function examples
+
+a = int(input("Enter number 1 : "))
+b = int(input("Enter number 2 : "))
+
+print("a value is ",a)
+print("b value is ",b)
+print("sum of a + b is ",a + b)
