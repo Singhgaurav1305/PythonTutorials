91ef23fe7d66d675ddda8644fef7a386fffea113
Gaurav Singh <thakurgaurav1305@gmail.com>
2026-02-06 10:47:45 +0530

    WAP find average of two numbers

---
+++ Chapter 2 - PS/Problem5.py
@@ -0,0 +1,6 @@
+# Program to find the average of two numbers entered by the user
+
+a = int(input("Enter first number : "))
+b = int(input("Enter Second number : "))
+avg = (a + b)/2
+print("Average of two number is : ",avg)
