f83aa886e9e3841bc361c160399a273dcad728cf
Gaurav Singh <thakurgaurav1305@gmail.com>
2026-02-06 08:41:50 +0530

    find the remainder

---
+++ Chapter 2 - PS/Problem2.py
@@ -0,0 +1,6 @@
+# Program to find the remainder when a number is divided by any other number
+# I have taken divisor as 2
+
+a = int(input("Enter the number : "))
+rem = a % 2
+print("The Remainder of given number is : ",rem)
